52a596c1b732aef3d1ed9c0de7fa4fe7207d63d2
Martin Bojnansky <martin.bojnansky@outlook.com>
2021-01-21 23:57:03 +0100

    Adds sample get/post calls

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sheetspi';
+  result = '';
+
+  constructor(protected httpClient: HttpClient) { }
+
+  get() {
+    this.httpClient.get('https://script.google.com/macros/s/AKfycbwC4dGDqixRvXo7JlJMa2A3HZVQO3ZwL3LfUh485yksoLvTx2Pdy9vo/exec').subscribe({
+      next: data => {
+        this.result = new Date().toString() + '' + JSON.stringify(data)
+      }
+    });
+  }
+
+  post() {
+    this.httpClient.post('https://script.google.com/macros/s/AKfycbwC4dGDqixRvXo7JlJMa2A3HZVQO3ZwL3LfUh485yksoLvTx2Pdy9vo/exec', null).subscribe({
+      next: data => {
+        this.result = new Date().toString() + '' + JSON.stringify(data)
+      }
+    });
+  }
 }
