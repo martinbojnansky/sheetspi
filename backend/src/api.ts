65121123706b732be4eed494eb137718ef568f7a
Martin Bojnansky <martin.bojnansky@outlook.com>
2021-01-21 18:42:21 +0100

    Adds deploy script and doPost action

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -3,5 +3,9 @@ export default {
     return ContentService
       .createTextOutput(JSON.stringify({ response: 'Hello, world!' }))
       .setMimeType(ContentService.MimeType.JSON);
+  },
+  doPost: (e: any) => {
+    return ContentService.createTextOutput(JSON.stringify({ response: 'Thanks!' }))
+      .setMimeType(ContentService.MimeType.JSON);
   }
 }
